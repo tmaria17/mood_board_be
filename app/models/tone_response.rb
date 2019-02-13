eba40612657eae3c98bdf65ad279e7ab5f17e891
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-13 14:17:09 -0700

    Fix model level validations on user, entries, and responses

--- app/models/tone_response.rb
+++ app/models/tone_response.rb
@@ -1,4 +1,4 @@
 class ToneResponse < ApplicationRecord
-  validates_presence_of :primary_tone, :secondary_tone
+  validates_presence_of :primary_tone, :primary_score
   belongs_to :journal_entry
 end
