940d156cd3b525196d40eb3dec17bfbe8b634ca2
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-12 21:24:15 -0700

    Set up tone response relationship testing.

--- app/models/tone_response.rb
+++ app/models/tone_response.rb
@@ -1,4 +1,4 @@
 class ToneResponse < ApplicationRecord
   validates_presence_of :primary_tone, :secondary_tone
-  belongs_to :journal_entries
+  belongs_to :journal_entry
 end
---
+++ spec/models/tone_response_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe ToneResponse do
+  describe 'validations' do
+    it {should validate_presence_of(:primary_tone)}
+    it {should validate_presence_of(:secondary_tone)}
+
+  end
+  describe 'relationships' do
+    it {should belong_to (:journal_entry)}
+  end
+end
