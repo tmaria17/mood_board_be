09e765ddd3c8375de2fab44754ca1bc2e9c04a44
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-12 21:13:15 -0700

    Create initial model spec for journal entry.

---
+++ spec/models/journal_entry_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe JournalEntry do
+  describe 'validations' do
+    it {should validate_presence_of(:entry_text)}
+  end
+  describe 'relationships' do
+   it {should have_many(:tone_responses)}
+  end
+end
