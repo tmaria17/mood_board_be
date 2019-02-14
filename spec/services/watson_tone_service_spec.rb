b7de290b660ae3cec7c3deb96845ad12ce94101a
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-14 16:43:47 -0700

    Create spec for service all tests passing.

--- spec/services/watson_tone_service_spec.rb
+++ spec/services/watson_tone_service_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 describe WatsonToneService do
   it 'exists' do
-      service = WatsonToneService.new("dogs")
-      binding.pry
+      service = WatsonToneService.new("dogs+are+awesome")
       expect(service).to be_a(WatsonToneService)
-    end
   end
+  it "returns a tone" do
+    service = WatsonToneService.new("dogs are awesome")
+
+    expect(service.get_tone).to be_a(Hash)
+    expect(service.get_tone).to have_key(:document_tone)
+    expect(service.get_tone[:document_tone]).to have_key(:tones)
+    expect(service.get_tone[:document_tone][:tones][0]).to have_key(:score)
+    expect(service.get_tone[:document_tone][:tones][0]).to have_key(:tone_id)
+    expect(service.get_tone[:document_tone][:tones][0]).to have_key(:tone_name)
+  end
+end
