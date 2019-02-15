81c265c12dfae83d10d2c9052e318a9a26d8e52e
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-15 12:43:23 -0700

    Format routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
+
   namespace :api do
     namespace :v1 do
       resources :users, only: [:show] do
@@ -11,5 +12,4 @@ Rails.application.routes.draw do
     end
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
