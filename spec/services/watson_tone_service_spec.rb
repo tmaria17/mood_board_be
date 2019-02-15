357acd894723d78f823347651513df545c2bae1a
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-14 22:15:15 -0700

    Fix mistake for Travis.

--- spec/services/watson_tone_service_spec.rb
+++ spec/services/watson_tone_service_spec.rb
@@ -6,7 +6,7 @@ describe WatsonToneService do
   end
   it "returns a tone" do
     service = WatsonToneService.new("dogs are awesome")
-
+    
     expect(service.get_tone).to be_a(Hash)
     expect(service.get_tone).to have_key(:document_tone)
     expect(service.get_tone[:document_tone]).to have_key(:tones)
