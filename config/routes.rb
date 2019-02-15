114eb04f3a61667a41e566bfe16f04309985544f
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-15 08:53:49 -0700

    Update routes for endpoints to be built

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:show] do
         get '/journal_entries', to: "journal_entries#show"
+        post '/journal_entries', to: "journal_entries#update"
+        resources :journal_entries, only: [:new, :create, :index]
+        resources :affirmations, only: [:new, :create, :show, :index]
       end
     end
   end
