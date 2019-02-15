601eb08ef0d435b04089f45f460986d174632cd6
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-15 12:48:55 -0700

    Fix wrong attributes in factories and update test

--- spec/requests/api/v1/user_journals/current_day_journal_spec.rb
+++ spec/requests/api/v1/user_journals/current_day_journal_spec.rb
@@ -4,7 +4,7 @@ require "date"
 describe "GET /api/v1/users/:id/journals?date=today" do
   it "Returns the journal entry for the current date" do
     user = create(:user)
-    user.journal_entries.create(created_at: Date.today)
+    journal_entry = create(:journal_entry, created_at: Date.today, user: user)
 
     params = {"date" => "today"}
 
@@ -19,6 +19,7 @@ describe "GET /api/v1/users/:id/journals?date=today" do
     expect(journal_response[:data]).to have_key :type
     expect(journal_response[:data][:attributes]).to have_key :created_at
     expect(journal_response[:data][:attributes]).to have_key :entry_text
+    expect(journal_response[:data][:attributes][:entry_text]).to_not be nil
     expect(journal_response[:data][:attributes]).to have_key :primary_score
     expect(journal_response[:data][:attributes]).to have_key :primary_tone
   end
