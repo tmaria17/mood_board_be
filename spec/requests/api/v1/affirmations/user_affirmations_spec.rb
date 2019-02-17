ffa0fd149dc3c9006091c940a93d2082a62feafb
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-17 15:23:55 -0700

    WIP affirmations.

--- spec/requests/api/v1/affirmations/user_affirmations_spec.rb
+++ spec/requests/api/v1/affirmations/user_affirmations_spec.rb
@@ -3,15 +3,18 @@ require "date"
 
 describe "GET /api/v1/users/:id/affirmations?date=YYYY-MM" do
   it "Returns the all affirmations for queired month" do
-    user = create(:user)
-    affirmation = create(:affirmation, created_at: Date.today, user: user)
+    user_1 = create(:user)
+    user_2 = User.new(name: "Jake", email: "ilikeshakes@gmail.com", password: "cat")
 
-    params = {"date" => "2019/02"}
+    affirmation_1 = create(:affirmation, created_at: Date.today, user: user_1)
+    affirmation_2 = create(:affirmation, created_at: Date.today, user: user_2)
+    affirmation_3 = create(:affirmation, created_at: Date.today, user: user_1)
 
-    get "/api/v1/users/#{user.id}/affirmations?data=YYYY-MM", :params => params
+
+    get "/api/v1/users/#{user_1.id}/affirmations?date=2019-02"
 
     affirmations = JSON.parse(response.body, symbolize_names: true)
-
+    binding.pry
     expect(affirmations).to have_key(:data)
   end
 end
