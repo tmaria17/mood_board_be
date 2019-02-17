27b136aab7a7c709ec81814c3983e15670f68080
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-17 16:41:52 -0700

    Fix service test for tone_analyzer and record VCR request

--- spec/services/watson_tone_service_spec.rb
+++ spec/services/watson_tone_service_spec.rb
@@ -4,17 +4,20 @@ describe WatsonToneService do
       service = WatsonToneService.new("dogs+are+awesome")
       expect(service).to be_a(WatsonToneService)
   end
-  xit "returns a tone" do
-    service = WatsonToneService.new("dogs are awesome")
+  it "returns a tone" do
+    VCR.use_cassette("watson_tone_request") do
+      service = WatsonToneService.new("dogs are awesome")
+      results = service.get_tone
 
-    expect(service.get_tone).to be_a(Hash)
-    expect(service.get_tone).to have_key(:document_tone)
-    expect(service.get_tone[:document_tone]).to have_key(:tones)
-    expect(service.get_tone[:document_tone][:tones][0]).to have_key(:score)
-    expect(service.get_tone[:document_tone][:tones][0]).to have_key(:tone_id)
-    expect(service.get_tone[:document_tone][:tones][0]).to have_key(:tone_name)
-    expect(service.get_tone[:document_tone][:tones][0][:tone_name]).to eq("Joy")
-    expect(service.get_tone[:document_tone][:tones][0][:tone_id]).to eq("joy")
-    expect(service.get_tone[:document_tone][:tones][0][:score]).to eq(0.928277)
+      expect(results).to be_a(Hash)
+      expect(results).to have_key(:document_tone)
+      expect(results[:document_tone]).to have_key(:tones)
+      expect(results[:document_tone][:tones][0]).to have_key(:score)
+      expect(results[:document_tone][:tones][0]).to have_key(:tone_id)
+      expect(results[:document_tone][:tones][0]).to have_key(:tone_name)
+      expect(results[:document_tone][:tones][0][:tone_name]).to eq("Joy")
+      expect(results[:document_tone][:tones][0][:tone_id]).to eq("joy")
+      expect(results[:document_tone][:tones][0][:score]).to eq(0.928277)
+    end
   end
 end
