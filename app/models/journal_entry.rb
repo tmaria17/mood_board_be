30a753be4ae0bdd9b2a970c4d4773dc271ed88cd
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-17 14:51:11 -0700

    Connect service to get requests to save and return tones and tone scores

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,4 +1,44 @@
 class JournalEntry < ApplicationRecord
   belongs_to :user
   has_one :tone_response
+
+  def self.find_user_daily_journal(user_id)
+    JournalEntry.where(user_id: user_id).find_by(created_at: Date.today)
+  end
+
+  def get_tone_results
+    results = tone_analyzer_results
+
+    tones = results[:document_tone][:tones]
+    update_data = {}
+    if tones.length == 1
+      update_data[:primary_tone] = tones.first[:tone_id]
+      update_data[:primary_score] = tones.first[:score]
+    else
+      update_data[:primary_tone] = tones.first[:tone_id]
+      update_data[:primary_score] = tones.first[:score]
+      update_data[:secondary_tone] = tones.last[:tone_id]
+      update_data[:secondary_score] = tones.last[:score]
+    end
+
+    ToneResponse.where(journal_entry_id: self.id).first_or_initialize.tap do |response|
+      response.update(update_data)
+      response.save
+    end
+  end
+
+  def entry_tones
+    res = ToneResponse.where(journal_entry_id: self.id).select(:primary_tone, :primary_score, :id)
+  end
+
+  private
+
+    def tone_analyzer_service
+      WatsonToneService.new(self.entry_text)
+    end
+
+    def tone_analyzer_results
+      tone_analyzer_service.get_tone
+    end
+
 end
--- spec/requests/api/v1/user_journals/update_current_day_journal_spec.rb
+++ spec/requests/api/v1/user_journals/update_current_day_journal_spec.rb
@@ -6,14 +6,6 @@ describe "PATCH /api/v1/users/:id/journals?date=today" do
     user = create(:user)
     journal_entry = create(:journal_entry, created_at: Date.today, user: user, entry_text: "I had such a bad day today.")
 
-    # update = { is_private: false }.to_json
-    #   patch(:update, update, id: 1)
-    #
-    #   get :show, id: 1, format: :json
-    #   assert_equal json['data'][0]['is_private'], false
-    #
-    # end
-
     params = { id: journal_entry.id, journal_entry: {entry_text: "I had a wonderful day today!"}}
 
     patch "/api/v1/users/#{user.id}/journal_entries?date=today", :params => params
@@ -28,7 +20,6 @@ describe "PATCH /api/v1/users/:id/journals?date=today" do
     expect(journal_response[:data][:attributes]).to have_key :created_at
     expect(journal_response[:data][:attributes]).to have_key :entry_text
     expect(journal_response[:data][:attributes][:entry_text]).to eq "I had a wonderful day today!"
-    expect(journal_response[:data][:attributes]).to have_key :primary_score
-    expect(journal_response[:data][:attributes]).to have_key :primary_tone
+    expect(journal_response[:data][:attributes]).to have_key :tones
   end
 end
