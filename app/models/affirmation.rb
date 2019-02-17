0ae54706ec8f8e82bdc7af56c2fa46041178cbfe
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-17 15:23:41 -0700

    Create a methods in affirmation controller to get only messages of affirmations.

--- app/models/affirmation.rb
+++ app/models/affirmation.rb
@@ -2,4 +2,18 @@ class Affirmation < ApplicationRecord
   validates_presence_of :affirmation_text
   belongs_to :user
 
+  # def self.get_text
+  #   affirmation_text_array = []
+  #   Affirmation.all.each do |affirmation|
+  #     affirmation_text_array.push(affirmation.affirmation_text)
+  #   end
+  #   affirmation_text_array
+  # end
+  # def get_text(user)
+  #   affirmation_text_array = []
+  #   Affirmation.where(user_id: user.id).each do |affirmation|
+  #     affirmation_text_array.push(affirmation.affirmation_text)
+  #   end
+  #   affirmation_text_array
+  # end
 end
