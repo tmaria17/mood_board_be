7c1682ccd9ed0a700c2d491c4ef9b9330b852893
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-17 15:23:09 -0700

    WIP Affirmation controller.

--- app/controllers/api/v1/affirmations_controller.rb
+++ app/controllers/api/v1/affirmations_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AffirmationsController < ApplicationController
   def index
-    render json: Affirmation.where(user_id: params[:user_id])
+    render json: AffirmationSerializer.new(Affirmation.where(user_id: params[:user_id]))
   end
 end
