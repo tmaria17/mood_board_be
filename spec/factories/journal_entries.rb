fe177821ee79a6ce53a2226ea8cdf0b38755fec0
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-18 12:51:59 -0700

    Write affirmation seeds for January 2019 and February 2019 for monthly affirmations endpoint

--- spec/factories/journal_entries.rb
+++ spec/factories/journal_entries.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :journal_entry do
     user { nil }
-    sequence :entry_text   { |n| "I am beautiful! #{n}" }
+    sequence :entry_text   { |n| "This is a journal entry example #{n}" }
   end
 end
