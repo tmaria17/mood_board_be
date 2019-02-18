28a51c2f04fa3e88821147c4775fa244213aa63e
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-18 15:40:02 -0700

    add date to attributes in serializer.

--- app/serializers/affirmation_wrapper_serializer.rb
+++ app/serializers/affirmation_wrapper_serializer.rb
@@ -1,6 +1,6 @@
 class AffirmationWrapperSerializer
   include FastJsonapi::ObjectSerializer
-  attributes
+  attributes :date
   attribute :affirmations do |object|
                 object.get_affirmation_text.map do |affirmation_obj|
                   Hash['affirmation_text', affirmation_obj]
