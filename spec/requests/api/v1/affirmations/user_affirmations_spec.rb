003602b55271bf52989c62a4d1cb8acca5437f0b
Maria Torres <25314178+tmaria17@users.noreply.github.com>
2019-02-18 18:19:16 -0700

    Create a test for affirmation POST request.

--- spec/requests/api/v1/affirmations/user_affirmations_spec.rb
+++ spec/requests/api/v1/affirmations/user_affirmations_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 require "date"
 
-describe "GET /api/v1/users/:id/affirmations?date=YYYY-MM" do
-  it "Returns the all affirmations for queired month" do
+describe "Affirmation Endpoints" do
+  it "it GETS affirmations" do
     user_1 = create(:user)
     user_2 = User.new(name: "Jake", email: "ilikeshakes@gmail.com", password: "cat")
 
@@ -21,5 +21,18 @@ describe "GET /api/v1/users/:id/affirmations?date=YYYY-MM" do
     expect(affirmations[:data][:type]).to eq("affirmation_wrapper")
     expect(affirmations[:data][:attributes]).to have_key(:affirmations)
     expect(affirmations[:data][:attributes][:affirmations].class).to eq(Array)
+  end
+  it 'POSTS affirmations' do
+    user_1 = create(:user)
+
+    post_params =  {
+      'affirmation_text': "I am feeling blue. "
+        }
+
+    post '/api/v1/users/:id/affirmations?date=today', params: post_params
+
+    # affirmation = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
   end
 end
