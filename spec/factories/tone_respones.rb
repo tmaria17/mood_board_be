0d88f2ae08d9f26979e92ce7bb19268372ff5d48
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-18 17:43:11 -0700

    Update factories

--- spec/factories/tone_respones.rb
+++ spec/factories/tone_respones.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :tone_response do
     journal_entry { nil }
-    primary_tone   { nil }
+    primary_tone   { ["anger", "joy", "sadness", "tentative", "analytical", "confident", "fear"].sample  }
     primary_score   { 0.88 }
   end
 end
