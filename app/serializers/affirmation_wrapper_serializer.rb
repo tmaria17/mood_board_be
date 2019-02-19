3ed5b1131ccc7e1e2df650d0361776be4315694c
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-19 15:33:21 -0700

    Edit serializer to lookup response by user

--- app/serializers/affirmation_wrapper_serializer.rb
+++ app/serializers/affirmation_wrapper_serializer.rb
@@ -4,10 +4,11 @@ class AffirmationWrapperSerializer
   attribute :affirmations do |object|
     object.affirmation_objects.map do |aff|
       hash = Hash.new
+      user = User.find(aff.user_id)
       date = (aff.created_at).strftime('%Y-%m-%d')
       hash[:date] = date
       hash[:affirmation_text] = aff.affirmation_text
-      hash[:tone] = ToneResponse.where(created_at: date).first.primary_tone
+      hash[:tone] = user.tone_responses.where(created_at: date).first.primary_tone
       hash
     end
   end
