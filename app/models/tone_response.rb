8af100a93433d571e645000eb8d8b2a816eafa3f
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-18 17:44:09 -0700

    Add methods to find DB objects by user_id and date

--- app/models/tone_response.rb
+++ app/models/tone_response.rb
@@ -1,4 +1,9 @@
 class ToneResponse < ApplicationRecord
   validates_presence_of :primary_tone, :primary_score
   belongs_to :journal_entry
+
+  def self.by_journal_entry(journal_entry_id)
+    self.find_by(journal_entry_id: journal_entry_id)
+  end
+
 end
