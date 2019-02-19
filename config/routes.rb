84f759922b5e907f5dc115ae4e23e0284a3f1604
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-19 11:10:52 -0700

    Fix merge rebase conflict

--- config/routes.rb
+++ config/routes.rb
@@ -9,14 +9,9 @@ Rails.application.routes.draw do
       resources :users, only: [:show] do
         get '/journal_entries', to: "journal_entries#show"
         patch '/journal_entries', to: "journal_entries#update"
-<<<<<<< HEAD
         get '/calendar', to: "calendar#show"
         resources :journal_entries, only: [:new, :create, :index]
         resources :affirmations, only: [:new, :create, :show, :index]
-=======
-        get '/calendar_day', to: "calendar_day#show"
-        resources :affirmations, only: [:create, :index]
->>>>>>> Add Crono gem to assist with Watson token refresh (future card)
       end
     end
   end
