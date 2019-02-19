b559e55b3599fc3ee7abb637e25570ac94cf96a4
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-18 17:42:54 -0700

    Add calendar route and write test for endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :users, only: [:show] do
         get '/journal_entries', to: "journal_entries#show"
         patch '/journal_entries', to: "journal_entries#update"
+        get '/calendar', to: "calendar#show"
         resources :journal_entries, only: [:new, :create, :index]
         resources :affirmations, only: [:new, :create, :show, :index]
       end
