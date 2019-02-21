62d6ea04529e9bbdce6aaafaa33a9cba47b342ff
Amy Leigh Petrie <apetrie9849@gmail.com>
2019-02-20 20:58:10 -0700

    Remove unnecessary lookup of user from serializer

--- app/serializers/affirmation_wrapper_serializer.rb
+++ app/serializers/affirmation_wrapper_serializer.rb
@@ -2,9 +2,10 @@ class AffirmationWrapperSerializer
   include FastJsonapi::ObjectSerializer
   attributes :date
   attribute :affirmations do |object|
+    user_id = object.affirmation_objects.first.user_id
+    user = User.find(user_id)
     object.affirmation_objects.map do |aff|
       hash = Hash.new
-      user = User.find(aff.user_id)
       date = (aff.created_at).strftime('%Y-%m-%d')
       hash[:date] = date
       hash[:affirmation_text] = aff.affirmation_text
